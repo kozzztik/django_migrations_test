94038d3c720f4c85284c06ecba9abe1cde5a413a
kozzztik <kozzztik@mail.ru>
2022-05-17 19:09:18 +0300

    Add field3

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 # Create your models here.
 class MyModel(models.Model):
     field1 = models.CharField(default="1", max_length=255)
+    field3 = models.CharField(default="1", max_length=255)
